caac62e483e4c2f2d3efcfc2ae55cfb4e3afcdbc
ShyrusThapa <shyrusthapa160@gmail.com>
2026-08-18 23:03:51 +0545

    1 23

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 let name="shyrus";
 let age= 21
 console.log(name, age)
+
+const roll=10;
+console.log(roll) //unchangable
+
+// old way var a=10
+
+
+//datatypes
+//primitive -- number, string, boolean, null,bigint,symbol
+
+let num=10;
+console.log(num);
+
+let c = "The name is Cyrus";
+console.log(c);
+
+let login= true;
+console.log(login);
+
+//bigint
+let num2=345368458n;
+console.log(num2);
