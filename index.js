8c66b9de4157c79937dd8444198a4b0a8d0c3efd
ShyrusThapa <shyrusthapa160@gmail.com>
2026-08-19 10:27:05 +0545

    formatting key value and function

--- index.js
+++ index.js
@@ -23,3 +23,42 @@ console.log(login);
 //bigint
 let num2=345368458n;
 console.log(num2);
+
+
+let obj= {
+    name:"Rolex",
+    account:007,
+    age:22,
+    category:'genz'
+}
+console.log(obj);
+
+
+//functions
+
+function add(){
+    console.log("Hello");
+}
+add();
+
+let s= function add(){
+    console.log("Hello World");
+}
+s();
+
+//type finding
+
+let x= 80;
+console.log(typeof x);
+
+let sname= "Bj";
+console.log(typeof sname);
+
+let x1= true;
+console.log("The type is" , typeof x1)
+
+//formating
+a= 20;
+b=30;
+c=a+b;
+console.log("The sum of " +  a + " and " + b +  " is " + c );
